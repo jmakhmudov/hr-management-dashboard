6e2d432eecea7e1071df732e59bb937b927c459f
jmakhmudov <makhmudov@tuta.io>
2023-08-14 16:27:50 +0500

    gh btn outline

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,7 +9,7 @@ const Navbar = () => {
         <nav className=" flex justify-between align-middle px-5 py-4">
             <Link href="/" className=" font-bold text-2xl text-blue-500">Human R.</Link>
             <div className=" flex align-middle gap-5">
-                <Button className=" font-semibold">
+                <Button className=" font-semibold" variant="outline">
                     <Link href="https://github.com/jmakhmudov/hr-management-dashboard" target="_blank">
                         Go to Repository <GitHubLogoIcon className=" inline mb-px"/>
                     </Link>
